f2886d45c5e82306cbec809953c7a47e1b963a31
Priyan Jeyaram <impriyanj@gmail.com>
2023-10-27 16:18:06 +0530

    Section 11

---
+++ Section11/forms-01-starting-setup/src/components/TheForm.vue
@@ -0,0 +1,174 @@
+<template>
+  <form @submit.prevent="submitData">
+    <!-- For TextBox: UserName -->
+    <div class="form-control" :class="{invalid: !userNameIsValid}">
+      <label for="user-name">Your Name</label>
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" @blur="validateInput"/>
+      <p v-if="!userNameIsValid">Please enter a valid name!</p>
+    </div>
+    <!-- For type-Number: UserAge -->
+    <div class="form-control">
+      <label for="age">Your Age (Years)</label>
+      <input id="age" name="age" type="number" v-model="userAge"/>
+    </div>
+    <!-- For  dropdown: referrer-->
+    <div class="form-control">
+      <label for="referrer">How did you hear about us?</label>
+      <select id="referrer" name="referrer" v-model="referrer">
+        <option value="google">Google</option>
+        <option value="wom">Word of mouth</option>
+        <option value="newspaper">Newspaper</option>
+      </select>
+    </div>
+    <!-- For checkBox: Interest -->
+    <div class="form-control">
+      <h2>What are you interested in?</h2>
+      <div>
+        <input id="interest-news" name="interest" type="checkbox" value="news" v-model="interest"/>
+        <label for="interest-news">News</label>
+      </div>
+      <div>
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tut" v-model="interest"/>
+        <label for="interest-tutorials">Tutorials</label>
+      </div>
+      <div>
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="interest"/>
+        <label for="interest-nothing">Nothing</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <h2>How do you learn?</h2>
+      <div>
+        <input id="how-video" name="how" type="radio" value="video" v-model="how"/>
+        <label for="how-video">Video Courses</label>
+      </div>
+      <div>
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how"/>
+        <label for="how-blogs">Blogs</label>
+      </div>
+      <div>
+        <input id="how-other" name="how" type="radio" value="other" v-model="how"/>
+        <label for="how-other">Other</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <the-rating-control v-model="rating"></the-rating-control>
+    </div>
+    <div class="form-control">
+      <input type="checkbox" value="confirm" name="confirm" v-model="confirm"/>
+      <label for="confirm">I agree to something I didn't read</label>
+    </div>
+    <div>
+      <button>Save Data</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import TheRatingControl from './TheRatingControl.vue';
+export default {
+  components: {
+    TheRatingControl
+  },
+  data() {
+    return {
+      userNameIsValid: true,
+      userName: '',
+      userAge: null,
+      referrer: 'google',
+      interest: [],
+      how: null,
+      confirm: false,
+      rating: null,
+    };
+  },
+  methods: {
+    submitData() {
+      console.log("user name: " + this.userName);
+      console.log("user age: ");
+      console.log(this.userAge);
+      console.log("referrer: " + this.referrer);
+      console.log("interest: "+ this.interest);
+      console.log("how: " + this.how);
+      console.log("confirm: " + this.confirm);
+      console.log("rating: " + this.rating);
+      this.rating = null;
+    },
+    validateInput() {
+      this.userName === '' ? this.userNameIsValid = false : this.userNameIsValid = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: #ffffff;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+.form-control.invalid label {
+  color: red;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+label {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+input,
+select {
+  display: block;
+  width: 100%;
+  font: inherit;
+  margin-top: 0.5rem;
+}
+
+select {
+  width: auto;
+}
+
+input[type='checkbox'],
+input[type='radio'] {
+  display: inline-block;
+  width: auto;
+  margin-right: 1rem;
+}
+
+input[type='checkbox'] + label,
+input[type='radio'] + label {
+  font-weight: normal;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #0076bb;
+  background-color: #0076bb;
+  color: white;
+  cursor: pointer;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+}
+
+button:hover,
+button:active {
+  border-color: #002350;
+  background-color: #002350;
+}
+</style>
---
+++ Section11/forms-01-starting-setup/src/components/TheRatingControl.vue
@@ -0,0 +1,59 @@
+<template>
+    <ul>
+        <li :class="{active: modelValue === 'poor'}">
+            <button type="button" @click="setRating('poor')">Poor</button>
+        </li>
+        <li :class="{active: modelValue === 'average'}">
+            <button type="button" @click="setRating('average')">Average</button>
+        </li>
+        <li :class="{active: modelValue === 'good'}">
+            <button type="button" @click="setRating('good')">Good</button>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['modelValue'],
+    emit:['update:modelValue'],
+
+    methods: {
+        setRating(modelValue) {
+            this.$emit('update:modelValue', modelValue);
+        }
+    }
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: #c2943f;
+}
+
+.active button {
+  color: #c2943f;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
