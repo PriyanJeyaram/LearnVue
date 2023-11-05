f186d1ad651077ea8b7030465621c88b2f261685
Priyan Jeyaram <impriyanj@gmail.com>
2023-11-05 11:14:35 +0530

    Registering Routes

---
+++ Section16/src/router.js
@@ -0,0 +1,17 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routers: [
+        {path: '/', redirect: '/coaches'},
+        {path: '/coaches', component: null},
+        {path: '/coaches/:id', component: null, children: [
+            {path: 'contact', component: null},
+        ]},
+        {path: '/register', component: null},
+        {path: '/requests', component: null},
+        {path: '/:notFound(.*)', component: null}
+    ],
+});
+
+export default router;
