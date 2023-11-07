a02c6833e173a4da7977c80e4b55323ddb598101
Priyan Jeyaram <impriyanj@gmail.com>
2023-11-07 19:06:30 +0530

    Changes before Deployment

--- Section16/src/router.js
+++ Section16/src/router.js
@@ -1,10 +1,10 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
-import CoachDetails from './pages/coaches/CoachDetails.vue';
+const CoachDetails = () => import('./pages/coaches/CoachDetails.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
 import store from './store/index.js';
